9c1a883c7cc28cab1a91df02ff6eb03ce33d6112
JorgeCuauro <jcuauro@gmail.com>
2023-06-19 10:42:38 -0400

    setup para trabajar local

--- catastro/settings.py
+++ catastro/settings.py
@@ -105,13 +105,19 @@ WSGI_APPLICATION = 'catastro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env('DB_NAME'),
+#         'USER': env('DB_USERNAME'),
+#        'PASSWORD': env('DB_PASSWORD'),
+#         'HOST': env('BD_HOST'),
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': env('DB_NAME'),
-         'USER': env('DB_USERNAME'),
-         'PASSWORD': env('DB_PASSWORD'),
-         'HOST': 'localhost',
+    "default": {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
